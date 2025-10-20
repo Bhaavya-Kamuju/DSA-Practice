bd89605d9200ac5d4b8e9f5f5b15acef34abfa6f
Bhaavya-Kamuju <22221a0540@gmail.com>
2025-10-20 21:39:47 +0530

    Find_missing_number_in Array.java

--- Find_missing_number_in Array.java
+++ Find_missing_number_in Array.java
@@ -54,5 +54,18 @@ class FindMissing{
         }
         return sum1-sum2;
     }
+    //Optimal approach 2 (TC =0(N), SC = O(1)) using XOR
+    /*
+    public static int findmissing(int[] arr,int n){
+        int xor1 =0 ;
+        int xor2=0;
+        for(int i=0;i<n-1;i++){
+            xor2 = xor2 ^ arr[i];
+            xor1 = xor1 ^ (i+1);
+        }
+        xor1 =xor1^n;
+        return (xor1^xor2);
+    }
+    */
 
 }
